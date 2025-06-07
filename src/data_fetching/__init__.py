ac787a61fef99c3368d83287ceb799bb4a797fb3
Harshit <52852877+ahv15@users.noreply.github.com>
2025-06-06 21:45:54 -0400

    Update data_fetching __init__.py to include LeetCode functionality

--- src/data_fetching/__init__.py
+++ src/data_fetching/__init__.py
@@ -1,5 +1,18 @@
-"""Data fetching modules for paper retrieval and processing"""
+"""
+Data Fetching Package
+
+This package contains modules for paper retrieval, processing, and LeetCode problem fetching.
+"""
 
 from .paper_fetcher import fetch_llm_sys_papers, fetch_arxiv_abstract
+from .leetcode_fetcher import get_problem, SeleniumLeetCodeFetcher, get_catalog, pick_random_problem, fetch_statement
 
-__all__ = ['fetch_llm_sys_papers', 'fetch_arxiv_abstract']
+__all__ = [
+    'fetch_llm_sys_papers', 
+    'fetch_arxiv_abstract',
+    'get_problem',
+    'SeleniumLeetCodeFetcher',
+    'get_catalog',
+    'pick_random_problem', 
+    'fetch_statement'
+]
